c1f3d07fca47e872b117ca1c05c1c6e53231d36e
Badre El-kdioui <badre.elkdioui@gmail.com>
2023-07-14 10:32:13 +0100

    still some norme and set all file of map in folder map

--- src/check_the_map.c
+++ src/check_the_map.c
@@ -6,7 +6,7 @@
 /*   By: bel-kdio <bel-kdio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 19:00:19 by bel-kdio          #+#    #+#             */
-/*   Updated: 2023/07/13 23:50:47 by bel-kdio         ###   ########.fr       */
+/*   Updated: 2023/07/14 10:30:25 by bel-kdio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	if_first_and_last(char **map, int i, int *j)
 	return (1);
 }
 
-int	if_bet_first_and_last(char **map, int i, int *j)
+int	if_bet_first_and_last(char **map, int i, int *j, int *player)
 {
 	while (map[i][*j] && map[i][*j] == ' ')
 		(*j)++;
@@ -35,11 +35,15 @@ int	if_bet_first_and_last(char **map, int i, int *j)
 	while (map[i][*j])
 	{
 		if (map[i][*j] != '1' && map[i][*j] != ' ' && map[i][*j] != '\n'
-				&& map[i][*j] != '0')
+				&& map[i][*j] != '0' && map[i][*j] != 'N' && map[i][*j] != 'S'
+				&& map[i][*j] != 'E' && map[i][*j] != 'W')
 			return (0);
 		if (map[i][*j] == ' ')
 			if (!space_is_protected(map, i, *j))
 				return (0);
+		if (map[i][*j] == 'N' || map[i][*j] == 'S' || map[i][*j] == 'E'
+			|| map[i][*j] == 'W')
+			(*player)++;
 		(*j)++;
 	}
 	return (1);
@@ -50,8 +54,10 @@ int	check_map(char **map)
 	int	num_lines;
 	int	i;
 	int	j;
+	int	player;
 
 	i = 0;
+	player = 0;
 	num_lines = cal_number_of_lines_map(map) - 1;
 	while (map[i])
 	{
@@ -62,10 +68,12 @@ int	check_map(char **map)
 				return (0);
 		}
 		else
-			if (!if_bet_first_and_last(map, i, &j))
+			if (!if_bet_first_and_last(map, i, &j, &player))
 				return (0);
 		i++;
 	}
+	if (player != 1)
+		return (0);
 	return (1);
 }
 
