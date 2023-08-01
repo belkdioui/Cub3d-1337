1fdf81431d6c1631dcb10b9e702ee268b98216d5
Rida Rhnizar <rrhnizar@c1r3p5.1337.ma>
2023-08-01 11:49:51 +0100

    handle Wall collisions.

--- src/draw_map/key_hooks_utils.c
+++ src/draw_map/key_hooks_utils.c
@@ -6,7 +6,7 @@
 /*   By: rrhnizar <rrhnizar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 17:19:17 by rrhnizar          #+#    #+#             */
-/*   Updated: 2023/08/01 10:03:26 by rrhnizar         ###   ########.fr       */
+/*   Updated: 2023/08/01 11:48:12 by rrhnizar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,16 @@
 
 void	key_w(t_map *map_draw, char **map, int tmp_xp, int tmp_yp)
 {
-	float degree = map_draw->rot_pl * (180 / M_PI);
 	tmp_yp = map_draw->y_p + map_draw->pdy;
 	tmp_xp = map_draw->x_p + map_draw->pdx;
-	// if (degree >= 0 && degree < 90)
-	// {
-	// 	tmp_yp += 2;
-	// 	tmp_xp += 2;
-	// }
-	// else if (degree >= 90 && degree < 180)
-	// {
-	// 	tmp_yp += 2;
-	// 	tmp_xp -= 2;
-	// }
-	// else if (degree >= 180 && degree < 270)
-	// {
-	// 	tmp_yp -= 2;
-	// 	tmp_xp -= 2;
-	// }
-	// else if (degree >= 270 && degree < 360)
-	// {
-	// 	tmp_yp -= 2;
-	// 	tmp_xp += 2;
-	// }
+
 	if (tmp_yp >= 0 && tmp_yp <= map_draw->h && tmp_xp >= 0
 		&& tmp_xp <= map_draw->w)
 	{
-		if (map[(tmp_yp) / 10][(tmp_xp) / 10] != '1')
+		if (map[(tmp_yp) / 10][(tmp_xp - 2) / 10] != '1' && 
+			map[(tmp_yp) / 10][(tmp_xp + 2) / 10] != '1' &&
+			map[(tmp_yp + 2) / 10][(tmp_xp) / 10] != '1' &&
+			map[(tmp_yp - 2) / 10][(tmp_xp) / 10] != '1')
 		{
 			map_draw->x_p_move += map_draw->pdx;
 			map_draw->y_p_move += map_draw->pdy;
@@ -50,34 +33,16 @@ void	key_w(t_map *map_draw, char **map, int tmp_xp, int tmp_yp)
 
 void	key_s(t_map *map_draw, char **map, int tmp_xp, int tmp_yp)
 {
-	float degree = map_draw->rot_pl * (180 / M_PI);
 	tmp_yp = map_draw->y_p - map_draw->pdy;
 	tmp_xp = map_draw->x_p - map_draw->pdx;
-	// if (degree >= 0 && degree < 90)
-	// {
-	// 	tmp_yp -= 2;
-	// 	tmp_xp -= 2;
-	// }
-	// else if (degree >= 90 && degree < 180)
-	// {
-	// 	tmp_yp -= 2;
-	// 	tmp_xp += 2;
-	// }
-	// else if (degree >= 180 && degree < 270)
-	// {
-	// 	tmp_yp += 2;
-	// 	tmp_xp += 2;
-	// }
-	// else if (degree >= 270 && degree < 360)
-	// {
-	// 	tmp_yp += 2;
-	// 	tmp_xp -= 2;
-	// }
 	
 	if (tmp_yp >= 0 && tmp_yp <= map_draw->h && tmp_xp >= 0
 		&& tmp_xp <= map_draw->w)
 	{
-		if (map[(tmp_yp) / 10][(tmp_xp) / 10] != '1')
+		if (map[(tmp_yp) / 10][(tmp_xp - 2) / 10] != '1' && 
+			map[(tmp_yp) / 10][(tmp_xp + 2) / 10] != '1' &&
+			map[(tmp_yp + 2) / 10][(tmp_xp) / 10] != '1' &&
+			map[(tmp_yp - 2) / 10][(tmp_xp) / 10] != '1')
 		{
 			map_draw->x_p_move -= map_draw->pdx;
 			map_draw->y_p_move -= map_draw->pdy;
@@ -87,35 +52,17 @@ void	key_s(t_map *map_draw, char **map, int tmp_xp, int tmp_yp)
 
 void	key_a(t_map *map_draw, char **map, int tmp_xp, int tmp_yp)
 {
-	float degree = map_draw->rot_pl * (180 / M_PI);
 	tmp_xp = map_draw->x_p + (map_draw->pdx * cos(-M_PI / 2) - map_draw->pdy
 			* sin(-M_PI / 2));
 	tmp_yp = map_draw->y_p + (map_draw->pdx * sin(-M_PI / 2) + map_draw->pdy
 			* cos(-M_PI / 2));
-	if (degree >= 0 && degree < 90)
-	{
-		tmp_yp -= 2;
-		tmp_xp += 2;
-	}
-	else if (degree >= 90 && degree < 180)
-	{
-		tmp_yp -= 2;
-		tmp_xp -= 2;
-	}
-	else if (degree >= 180 && degree < 270)
-	{
-		tmp_yp += 2;
-		tmp_xp -= 2;
-	}
-	else if (degree >= 270 && degree < 360)
-	{
-		tmp_yp -= 2;
-		tmp_xp -= 2;
-	}
 	if (tmp_yp >= 0 && tmp_yp <= map_draw->h && tmp_xp >= 0
 		&& tmp_xp <= map_draw->w)
 	{
-		if (map[(tmp_yp) / 10][(tmp_xp) / 10] != '1')
+		if (map[(tmp_yp) / 10][(tmp_xp - 2) / 10] != '1' && 
+			map[(tmp_yp) / 10][(tmp_xp + 2) / 10] != '1' &&
+			map[(tmp_yp + 2) / 10][(tmp_xp) / 10] != '1' &&
+			map[(tmp_yp - 2) / 10][(tmp_xp) / 10] != '1')
 		{
 			map_draw->x_p_move += (map_draw->pdx * cos(-M_PI / 2)
 					- map_draw->pdy * sin(-M_PI / 2));
@@ -127,35 +74,18 @@ void	key_a(t_map *map_draw, char **map, int tmp_xp, int tmp_yp)
 
 void	key_d(t_map *map_draw, char **map, int tmp_xp, int tmp_yp)
 {
-	float degree = map_draw->rot_pl * (180 / M_PI);
 	tmp_xp = map_draw->x_p + (map_draw->pdx * cos(M_PI / 2) - map_draw->pdy
 			* sin(M_PI / 2));
 	tmp_yp = map_draw->y_p + (map_draw->pdx * sin(M_PI / 2) + map_draw->pdy
 			* cos(M_PI / 2));
-	if (degree >= 0 && degree < 90)
-	{
-		tmp_yp += 2;
-		tmp_xp -= 2;
-	}
-	else if (degree >= 90 && degree < 180)
-	{
-		tmp_yp += 2;
-		tmp_xp += 2;
-	}
-	else if (degree >= 180 && degree < 270)
-	{
-		tmp_yp -= 2;
-		tmp_xp += 2;
-	}
-	else if (degree >= 270 && degree < 360)
-	{
-		tmp_yp += 2;
-		tmp_xp += 2;
-	}
+	
 	if (tmp_yp >= 0 && tmp_yp <= map_draw->h && tmp_xp >= 0
 		&& tmp_xp <= map_draw->w)
 	{
-		if (map[(tmp_yp) / 10][(tmp_xp) / 10] != '1')
+		if (map[(tmp_yp) / 10][(tmp_xp - 2) / 10] != '1' && 
+			map[(tmp_yp) / 10][(tmp_xp + 2) / 10] != '1' &&
+			map[(tmp_yp + 2) / 10][(tmp_xp) / 10] != '1' &&
+			map[(tmp_yp - 2) / 10][(tmp_xp) / 10] != '1')
 		{
 			map_draw->x_p_move += (map_draw->pdx * cos(M_PI / 2)
 					- map_draw->pdy * sin(M_PI / 2));
